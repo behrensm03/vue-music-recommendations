177ac2d59019b4931bf53b2dac6051efe385c7dc
behrensm03 <behrensm03@gmail.com>
2024-11-10 23:10:34 -0500

    chore: add docs (#6)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -15,7 +15,7 @@ const goToHome = () => {
         This system is designed to provide NLP-based recommendations for musicians based on user input of musicians they already enjoy. It works by computing similarity scores between the input musicians and other musicians in the system. The similarity score between two musicians is defined as the cosine similarity between the vectorized Wikipedia biographies of the two musicians.
       </p>
       <br />
-      <a href="../assets/description.pdf">Read a more in depth description here.</a>
+      <a href="/Music_Recommendation_Background.pdf">Read a more in depth description here.</a>
       <br />
       <div :class="$style.link" @click="goToHome">Back to home</div>
     </div>
